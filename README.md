9bacc42652bd098178e6039bd64617f80ab4e97b
Oleksandr Pidhurskyi <opidhurskyi@gmail.com>
2025-06-03 09:54:50 +0300

    http server basic functional added

---
+++ README.md
@@ -0,0 +1,11 @@
+Backup application for windows system
+
+
+Backup methods:
+1. Lockal backup #in progress
+2. S3 backup #will be realized in feature
+3. SMB\CIFS backup #will be realized in feature
+
+Interface:
+1. Web Interface #in progress
+2. CLI #will be realized in feature
--- cmd/backup-app/main.go
+++ cmd/backup-app/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
+	"time"
 
 	"gopkg.in/yaml.v3"
 )
@@ -21,8 +26,86 @@ func main() {
 	fmt.Printf(" Backup Directory: %s\n", cfg.BackupDir)
 	fmt.Printf(" Path to DataBase: %s\n", cfg.DatabasePath)
 	fmt.Printf(" Secret salt (first 5 symbols): %s...\n", cfg.SecretSalt[:5])
+	fmt.Printf(" Server Timeouts: Read=%s, Write=%s, Idle=%s\n", cfg.ReadTimeout, cfg.WriteTimeout, cfg.IdleTimeout)
+	fmt.Printf(" Shutdown Timeout: %s\n", cfg.ShutdownTimeout)
 
-	fmt.Println("\nApplication successfully loaded. Server will be run next...")
+	//Own multiplexor creating (router)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthHandler)
+	mux.HandleFunc("status", statusHandler)
+
+	//Timeouts parsing
+	readTimeout, err := time.ParseDuration(cfg.ReadTimeout)
+	if err != nil {
+		log.Fatalf("Wrong format ReadTimeout: %v", err)
+	}
+
+	writeTimeout, err := time.ParseDuration(cfg.WriteTimeout)
+	if err != nil {
+		log.Fatalf("Wrong format WriteTimeout: %v", err)
+	}
+
+	idleTimeout, err := time.ParseDuration(cfg.IdleTimeout)
+	if err != nil {
+		log.Fatalf("Wrong format IdleTimeout: %v", err)
+	}
+
+	shutdownTimeout, err := time.ParseDuration(cfg.ShutdownTimeout)
+	if err != nil {
+		log.Fatal("Wrong format ShutdownTimeout: %v", err)
+	}
+
+	//Create and configure http.Server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.ServerPort),
+		Handler:      mux, //use our own multiplexor
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeout,
+		ErrorLog:     log.New(os.Stderr, "HTTP-Server Error: ", log.LstdFlags),
+	}
+
+	//System sygnals channels and channels for launch error messages
+	serverErrors := make(chan error, 1)
+	osSignals := make(chan os.Signal, 1)
+
+	//Register signals that we want to listen to (Ctrl+C, SIGTERM)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
+
+	//HTTP server launch in separated goroutine
+	go func() {
+		log.Printf("Web Server launch on %s...\n", srv.Addr)
+		serverErrors <- srv.ListenAndServe()
+	}()
+
+	/* if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("HTTP-server launch error: %v", err)
+	} */
+
+	//Waiting for system signal or server error
+	select {
+	case err := <-serverErrors:
+		//Server returns an error
+		if err != http.ErrServerClosed {
+			log.Fatalf("HTTP-server launch error: %v", err)
+		}
+	case sig := <-osSignals:
+		//Received system signal
+		log.Printf("Received system signal: %v. Begin gracefull shutdown...", sig)
+
+		//Create context with timeout for operation fnish
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+		defer cancel()
+
+		//Initialize gracefull shutdown
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.Printf("Gracefull shutdown error: %v", err)
+			os.Exit(1)
+		}
+		log.Println("Server shutdown gracefully")
+	}
+
+	log.Println("Application shutdown.")
 }
 
 type Config struct {
@@ -30,6 +113,12 @@ type Config struct {
 	BackupDir    string `yaml:"backup_directory"`
 	DatabasePath string `yaml:"database_path"`
 	SecretSalt   string `yaml:"secret_salt"`
+
+	ReadTimeout  string `yaml:"read_timeout"`
+	WriteTimeout string `yaml:"write_timeout"`
+	IdleTimeout  string `yaml:"idle_timeout"`
+
+	ShutdownTimeout string `yaml:"shutdown_timeout"`
 }
 
 func LoadConfig() (*Config, error) {
@@ -47,4 +136,15 @@ func LoadConfig() (*Config, error) {
 	}
 
 	return &cfg, nil
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Application works correct! Time %s", time.Now().Format("2006-01-02 15:04:05"))
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(3 * time.Second)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Overal backup status: Vaiting for main functional realization. (GET /status)")
 }
